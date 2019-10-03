a0ec11bf47aa937645680cfc32cb488432902b27
Igor Martucelli <martucelli.igor@gmail.com>
2019-10-03 03:33:55 -0300

    Scope: CREATE ROUTE TO EXPOSE FILES TO FRONT-END

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -7,6 +7,14 @@ const SpotSchema = new mongoose.Schema(
     thumbnail: String,
     techs: [ String],
     user: { type: mongoose.Schema.Types.ObjectId }
+}, {
+    toJSON: {
+        virtuals: true
+    }
 });
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`;
+})
 
 module.exports = mongoose.model("Spot", SpotSchema);
